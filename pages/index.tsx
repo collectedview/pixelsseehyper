7fef7b59c4617dfd5a9c9545d1258e750f747863
Joe Alongi <collectedview@icloud.com>
2023-03-18 19:17:25 -0400

    Landing Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,29 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>pixelssh</title>
-        <meta name="description" content="pixelssh" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main >
-      <div >
-            <Image
-              src="/pixelssh.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-      </main>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>pixelssh</title>
+                <meta name="description" content="pixelssh" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/pixelssh.ico" />
+            </Head>
+            <main>
+                <div className="container">
+                    <Image
+                        className="logo"
+                        src="/pixelssh.svg"
+                        alt="pixelssh"
+                        width={100}
+                        height={100}
+                        priority
+                    />
+                </div>
+            </main>
+        </>
+    )
 }
