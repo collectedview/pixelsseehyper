1e181a7897ebaacc868112d31ce94aa81c64a1b9
Joe Alongi <collectedview@icloud.com>
2023-05-21 00:11:24 -0400

    Remove Facing Mode

--- ui/components/viewer.tsx
+++ ui/components/viewer.tsx
@@ -7,7 +7,6 @@ export default function Viewer() {
     const { media } = StreamMedia({
         audio: true,
         video: {
-            facingMode: 'user',
             width: { min: 1280, ideal: 1920, max: 3840 },
             height: { min: 720, ideal: 1080, max: 2160 },
             frameRate: { min: 30, ideal: 60, max: 120 },
